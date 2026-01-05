34f319766fe51d0ac4ab6a23f398081fe6c8455f
TridevBalaji <151658450+TridevBalaji@users.noreply.github.com>
2026-01-05 10:20:25 +0530

    Add variables for EKS configuration in Terraform

---
+++ variables.tf
@@ -0,0 +1,41 @@
+variable "region" {
+  description = "AWS region where EKS will be created"
+  type        = string
+  default     = "ap-southeast-2"
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "jenkins-eks"
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version for EKS"
+  type        = string
+  default     = "1.29"
+}
+
+variable "node_instance_type" {
+  description = "EC2 instance type for EKS worker nodes"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "desired_capacity" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "min_capacity" {
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 1
+}
+
+variable "max_capacity" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 3
+}
